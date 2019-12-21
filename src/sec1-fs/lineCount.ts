4c4383c03900970b677116bfc7055c7fc4f179d1
Masaharu Inoue <pasteur1822@gmail.com>
2019-12-21 20:16:16 +0900

    fix: コメント修正

--- src/sec1-fs/lineCount.ts
+++ src/sec1-fs/lineCount.ts
@@ -4,6 +4,6 @@
 import * as fs from 'fs'
 
 export default function lineCount(path: string): number {
-  const data = fs.readFileSync(path, 'utf-8') // ■このメソッドをmock化したい
+  const data = fs.readFileSync(path, 'utf-8') // このメソッドをmock化したい
   return data.split('\n').length
 }
